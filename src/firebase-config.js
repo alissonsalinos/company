f106b663ce0911905cc011be8bed1ada37a032f0
Alisson <contato@alissondias.com>
2018-12-09 22:52:15 -0200

    Enviando o projeto faltando a funcionalidade de login e spiner para o envio de dados

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+const Rebase = require('re-base')
+const firebase = require('firebase')
+
+
+const FirebaseConfig = {
+    apiKey: "AIzaSyDfFVHqIbAsDJlA2DGkYLKte6fSeuyos5c",
+    authDomain: "alisson-portfolio.firebaseapp.com",
+    databaseURL: "https://alisson-portfolio.firebaseio.com",
+    projectId: "alisson-portfolio",
+    storageBucket: "alisson-portfolio.appspot.com",
+    messagingSenderId: "538188953608"
+  }
+  
+
+  const app = firebase.initializeApp(FirebaseConfig)
+  const config = Rebase.createClass(app.database())
+
+  export const storage = app.storage()
+
+  export default config
